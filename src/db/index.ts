4da809f36c3761b60180d8fdcc189a471676f8f8
pranta-barua007 <prantabarua247@gmail.com>
2025-12-20 15:34:08 +0600

    chore(db): dual driver support for local/production
    Updated db configuration to use @vercel/postgres by default (production) and fall back to local pg pool in non-production environments.

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,18 +1,25 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
+import { drizzle as drizzlePg } from 'drizzle-orm/node-postgres';
+import { drizzle as drizzleVercel } from 'drizzle-orm/vercel-postgres';
+import { sql } from '@vercel/postgres';
 import { Pool } from 'pg';
 import * as schema from "./schema";
-
 
 const globalForDb = globalThis as unknown as {
     conn: Pool | undefined;
 };
-// config({ path: '.env.local' }); // or .env
 
-const conn = globalForDb.conn ?? new Pool({
-    connectionString: process.env.POSTGRES_URL!,
-});
+let db: ReturnType<typeof drizzlePg> | ReturnType<typeof drizzleVercel>;
 
+db = drizzleVercel(sql, { schema });
 
-if (process.env.NODE_ENV !== "production") globalForDb.conn = conn;
+if (process.env.NODE_ENV !== 'production') {
+    const conn = globalForDb.conn ?? new Pool({
+        connectionString: process.env.POSTGRES_URL!,
+    });
 
-export const db = drizzle(conn, { schema });
+    globalForDb.conn = conn;
+
+    db = drizzlePg(conn, { schema });
+}
+
+export { db };
